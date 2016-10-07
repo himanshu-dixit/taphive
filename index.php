0f3045f62f614ba3f643aadb1568c885665f99ff
Himanshu Dixit <hudixt@gmail.com>
2016-10-07 23:51:23 +0530

    first

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <title>Code Compare</title>
+  <meta charset="utf-8" />
+<meta name="description" content="Code and Compete With Friends and Peers. Showcase your skills.">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+ <link rel="stylesheet" href="assests/index.css">
+
+
+</head>
+<body>
+
+    <div class="intro_bar" id="scaled_intro_bar">
+      <div class="blue_layer">
+
+        <div class="container">
+            <div class="row">
+        <nav class="nav">
+       <div class="container-fluid">
+         <div class="navbar-header">
+           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+           </button>
+           <a class="navbar-brand logo" href="https://himanshudixit.me">CODELYTICS</a>
+         </div>
+         <div class="collapse navbar-collapse" id="myNavbar">
+         <ul class="nav navbar-nav navbar-right">
+           <li class="active"><a href="https://himanshudixit.me">Home</a></li>
+           <li><a href="https://himanshudixit.me/about_me.html">About Me</a></li>
+           <li><a href="https://himanshudixit.me/skills.html">Skills</a></li>
+
+         </ul>
+       </div>
+       </div>
+     </nav>
+   </div></div>
+
+
+
+      </div>
+
+    </div>
+
+
+<div class="content">
+  <div class="container">
+
+
+
+</div>
+</div>
+
+<script>
+document.getElementById("scaled_intro_bar").style.height = $(window).height()+"px";
+console.log($(window).height());
+</script>
+</body>
+</html>
